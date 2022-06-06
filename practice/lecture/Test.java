eba9ca47f2fa6e5482e3b0c83f1b6de3ea3efe1b
EmilRR24 <grimlei2005@yahoo.com>
2022-06-06 11:20:14 -1000

    created a Test java from lecture

---
+++ practice/lecture/Test.java
@@ -0,0 +1,28 @@
+// file name and class name must match
+// complied language 
+// 1. compile -> .class file 2. run the .class file
+// for pillars of OOP - Inheritance - Abstraction - Polymorphism - Encapsulation
+// Strongly typed language (data type has to be explicitly mentioned)
+// no functions because of class, everything is a method of a class
+// var firstName = "Ed"
+// firstName = 23 // would cause an error because switching from string to integer
+// string is object and has ""
+// String firstName = "Ed"
+// char is primitive and has ''
+// char letter = 'c'
+// 2 types of casting Explicit vs Implicit
+//Implicit - simpler data type -> complex data type
+// double accurateAge = age;
+//Explicit - complex data type -> simple data type
+// double price = 21.33
+// int wholePrice = (int)price;
+
+public class Test{
+  // POINT OF ENTRY METHOD`
+  public static void main(String args[]){
+    String name ="Ed";
+    int age = 32;
+    System.out.println(name);
+    System.out.println("Hello World");
+  }
+}
