8b2995dfb3c666fb92ba8afe072b4ebd35c40911
EmilRR24 <grimlei2005@yahoo.com>
2022-06-06 17:39:30 -1000

    completed assignment cafe_business_logic

--- assignments/business_logic/CafeUtil.java
+++ assignments/business_logic/CafeUtil.java
@@ -10,16 +10,32 @@ public class CafeUtil {
       // total = total + i;
     }
     return total;
-  // // METHOD SIGNATURE - double
-  // public double getOrderTotal(double[] prices){
-
-  // }
-  // // METHOD SIGNATURE - void
-  // public void displayMenu(ArrayList<String> menuItems){
-
-  // }
-  // // METHOD SIGNATURE - undefined (no return)
-  // public void addCustomer(ArrayList<String> customers){
-
+  }
+  // METHOD SIGNATURE - double
+  public double getOrderTotal(double[] prices){
+    double sum = 0.0;
+    int i = 1;
+    while(i < prices.length){
+      sum += prices[i];
+      i++;
+    }
+    return sum;
+  }
+  // METHOD SIGNATURE - void
+  public void displayMenu(ArrayList<String> menuItems){
+    int i = 0;
+    while( i < menuItems.size()){
+      System.out.println(i + " " + menuItems.get(i));
+      i++;
+    }
+  }
+  // METHOD SIGNATURE - undefined (no return)
+  public void addCustomer(ArrayList<String> customers){
+    System.out.println("Please enter your name:");
+    String userName = System.console().readLine();
+    System.out.println("Hello, " + userName + "!");
+    System.out.println("There are " + customers.size() + " people in front of you.");
+    customers.add(userName);
+    System.out.println(customers);
   }
 }
