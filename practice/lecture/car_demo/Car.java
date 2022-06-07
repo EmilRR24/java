305877137ed8e99aa78b17b6530f6cb13678e106
EmilRR24 <grimlei2005@yahoo.com>
2022-06-07 07:56:01 -1000

    car_demo folder created to hold car and testcar

--- practice/lecture/car_demo/Car.java
+++ practice/lecture/car_demo/Car.java
@@ -11,6 +11,7 @@ public class Car{
     private String color;
     private String engine;
     private boolean convertible;
+    private int odometer;
     // CONSTRUCTOR METHODS
     // YOU CAN OVERLOAD THE METHODS AND CREATE AS MANY CONSTRUCTORS AS YOU WANT
     // EMPTY CONSTRUCTOR - no parameters passed by user
@@ -21,6 +22,7 @@ public class Car{
         this.color = "Black";
         this.engine = "V4";
         this.convertible = false;
+        this.odometer = 0;
     }
     // FULL CONSTRUCTOR - all paramerters passed by user
     public Car(int wheels, int doors, String color, String engine, boolean convertible){
@@ -30,6 +32,7 @@ public class Car{
         this.color = color;
         this.engine = engine;
         this.convertible = convertible;
+        this.odometer = 0;
     }
     // GETTERS / SETTERS / OTHER METHODS
     // GETTERS
@@ -41,6 +44,16 @@ public class Car{
     }
     //SETTERS
     public void setWheels(int newWheels){
-        this.wheels = this.newWheels;
+        this.wheels = newWheels;
     }
+    // OTHER METHODS
+    // DRIVE
+    public Car drive(int distance, int speed){
+        this.odometer += distance;
+        System.out.println(this.odometer);
+        return this;
+    }
+    // HONK
+
+    // START
 }
